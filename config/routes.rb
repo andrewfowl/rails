c449b0b9d84c9da6e0138effe4c60072f73b6e31
andrewfowl <andrewfowl@mail.ru>
2023-05-26 10:33:27 -0400

    Part 19

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,15 @@ Rails.application.routes.draw do
 
   get "about", to: redirect('/about-us')
 
+  get "password", to: "passwords#edit", as :edit_password
+  patch "password", to: "passwords#update"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
