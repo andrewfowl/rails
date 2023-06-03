0e6c4ba3900dc111352f6ced740b90c14c479b2e
andrewfowl <andrewfowl@mail.ru>
2023-06-03 02:09:02 -0400

    Part 30

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,7 +9,22 @@ class TweetsController < ApplicationController
         @tweet = tweet.new
     end
 
+    def create
+        @tweet = Current.user.tweets.create(tweet_params)
+        if @tweet.save
+            redirect_to tweets_path, notice: "Tweet scheduled successfully"
+        else
+            render :new
+        end
+    end
+
     def destroy
+    end
+
+    private
+
+    def tweet_params
+        params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
     end
 
 end
